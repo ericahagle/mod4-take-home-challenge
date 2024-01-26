9119e80602313a64e3e5daa99be0f598ee84c9b5
Erica Hagle <ericahagle.dev@gmail.com>
2024-01-26 14:16:25 -0800

    feat: Add styling for ArticleDetail; add no-image-available logic

--- src/components/ArticlePreview/ArticlePreview.jsx
+++ src/components/ArticlePreview/ArticlePreview.jsx
@@ -4,16 +4,17 @@ import './ArticlePreview.css';
 const ArticlePreview = ({ date, title, description, imageUrl, source }) => {
   return (
     <div className='ArticlePreview'>
-      {imageUrl && <img className='preview-image' src={imageUrl} alt={title} />}
-        <div className='preview-short-deets'>
-          <p className='preview-date'>{date}</p>
-          <p className='preview-source'>{source}</p>
-        </div>
-        <div className='preview-long-deets'>
-          <h2 className='preview-title'>{title}</h2>
-          <p className='preview-description'>{description}</p>
-        </div>
+      {imageUrl ? (<img className='preview-image' src={imageUrl} alt={title} />) : (<img className='preview-image' src={`${process.env.PUBLIC_URL}/no-image-available.jpeg`} alt='No Image Available' />)}
+      <div className='preview-short-deets'>
+        <p className='preview-date'>Publish Date: {date}</p>
+        <p className='preview-source'>Source: {source}</p>
       </div>
+      <div className='preview-long-deets'>
+        <h2 className='preview-title'>{title}</h2>
+        <p className='preview-description'>{description}</p>
+        <p className='preview-click-for-more'>Click to see more.</p>
+      </div>
+    </div>
   );
 };
 
